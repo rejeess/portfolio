81f04cf4c4e692249b8b753306c515178c14feb0
Rejeess Bhose <rejeessbhose@Rejeesss-MacBook-Pro.local>
2020-11-16 13:10:42 -0600

    contact form and about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,48 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+
+const About = ({
+  data:{
+    about: {nodes},
+  },
+}) => {
+  const {title, intro, image} = nodes[0]
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img"/>
+          <article className="about-text">
+            <Title title={title}/>
+            <p>{intro}</p>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+
+export const query = graphql`
+  {
+    about:allStrapiAbout {
+      nodes {
+        title
+        intro
+        id
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+
 
 export default About
